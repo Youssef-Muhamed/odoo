07e00146a78d8c695657ccac27c7bdad6c8919d4
youssef <eng.youssef2023@gmail.com>
2023-03-26 17:25:18 +0200

    lap 4

--- odoo15/customemodule/hms/__manifest__.py
+++ odoo15/customemodule/hms/__manifest__.py
@@ -2,8 +2,14 @@
     'name':'Hms Patient',
     'version':'1.0.0',
     'data':[
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/hms_view.xml',
         'views/hms_department.xml',
         'views/hms_doctors.xml',
         'views/hms_patient.xml',
-    ]
+        'reports/report.xml',
+        'reports/templates.xml',
+    ],
+    'depends': ['base',]
 }
